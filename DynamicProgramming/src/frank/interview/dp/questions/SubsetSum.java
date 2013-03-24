75cbcc4e857ec32e1a0843bb76a949d7fea8ab3a
CodeMonkey9527 <frank.shishen@gmail.com>
2013-03-23 19:10:31 -0700

    implement subset sum

--- DynamicProgramming/src/frank/interview/dp/questions/SubsetSum.java
+++ DynamicProgramming/src/frank/interview/dp/questions/SubsetSum.java
@@ -1,5 +1,8 @@
 package frank.interview.dp.questions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given an array of positive integers, find a subset of the array which sums up to another number
  * Example: 
@@ -17,4 +20,98 @@ package frank.interview.dp.questions;
  */
 public class SubsetSum {
 
+	private static int total(int [] array) {
+		int total = 0;
+		for (int n : array) {
+			total += n;
+		}
+		return total;
+	}
+	
+	public static List<Integer> find(int [] array, int num) {
+		if (array == null || array.length == 0) {
+			return null;
+		}
+		if (num == 0) {
+			// return empty set if target is 0
+			return new ArrayList<Integer>();
+		}
+		String failureOutput = String.format("Can't find subset for %d", num);
+		if (num < array[0]) {
+			System.out.println(failureOutput);
+			return null;
+		}
+		int maxSum = total(array);
+		if (num > maxSum) {
+			System.out.println(failureOutput);
+			return null;
+		}
+		int length = array.length;
+		boolean [][] result = new boolean[length][num + 1];
+		int [][] back = new int [length][num + 1];
+		// initial cases
+		for (int i = 0; i <= num; i++) {
+			result[0][i] = false;
+			back[0][i] = 0;
+		}
+		for (int i = 0; i < length; i++) {
+			result[i][0] = true;
+			back[i][0] = 1;
+		}
+		result[0][array[0]]  = true;
+		back[0][array[0]] = 2;
+		
+		// going forward
+		for (int i = 1; i < length; i++) {
+			for (int s = 0; s <= num; s++) {
+				if (result[i-1][s]) {
+					// if s can be sum'ed to by a1, a2, ... , ai-1
+					result[i][s] = true;
+					back[i][s] = 1;
+				} else if (s >= array[i] && result[i-1][s-array[i]]) {
+					// if s-ai can be sum'ed to by a1, a2, ... , ai-1
+					result[i][s] = true;
+					back[i][s] = 2;
+				} else {
+					result[i][s] = false;
+					back[i][s] = 0;
+				}
+			}
+		}
+		
+		// going backward
+		if (result[length - 1][num] == false) {
+			System.out.println(failureOutput);
+			return null;
+		}
+		List<Integer> subset = new ArrayList<Integer>();
+		int n = length - 1;
+		int s = num;
+		while(n >= 0) {
+			if (back[n][s] == 1) {
+				n--;
+			} else if (back[n][s] == 2) {
+				subset.add(array[n]);
+				s -= array[n];
+				n--;
+			} else {
+				throw new IllegalStateException("Impossible!");
+			}
+		}
+		
+		StringBuffer output = new StringBuffer();
+		output.append(num);
+		output.append("=");
+		if (subset.size() > 0) {
+			output.append(subset.get(0));
+		}
+		for (int i = 1; i < subset.size(); i++) {
+			output.append("+");
+			output.append(subset.get(i));
+		}
+		System.out.println(output);
+		
+		return subset;
+	}
+	
 }
--- DynamicProgramming/tst/frank/interview/dp/questions/test/SubsetSumTest.java
+++ DynamicProgramming/tst/frank/interview/dp/questions/test/SubsetSumTest.java
@@ -1,6 +1,58 @@
 package frank.interview.dp.questions.test;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import frank.interview.dp.questions.SubsetSum;
 
 public class SubsetSumTest {
 
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testFind() {
+		System.out.println("------Test 1------");
+		int [] array = {1, 2, 4, 8, 16, 32};
+		for (int num=0; num<64; num++) {
+			List<Integer> subset = SubsetSum.find(array, num);
+			assertNotNull(subset);
+			int sum = 0; 
+			for (Integer i : subset) {
+				sum += i;
+			}
+			assertEquals(num, sum);
+		}
+		for (int num=64; num<70; num++) {
+			assertNull(SubsetSum.find(array, num));
+		}
+	}
+
+	@Test
+	public void testFind2() {
+		System.out.println("------Test 2------");
+		int [] array = {1, 3, 5, 7, 9};
+		for (int num = 0; num < 30; num++) {
+			SubsetSum.find(array, num);
+		}
+	}
+	
+	@Test
+	public void testFind3() {
+		System.out.println("------Test 3------");
+		int [] array = {1, 2, 3, 4, 5, 6, 7};
+		for (int num = 0; num < 40; num++) {
+			SubsetSum.find(array, num);
+		}
+	}
 }
